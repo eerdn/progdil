72444ed95e2579b2e8cb5d4551d76d40112c734f
Esra ERDEN <esra.erden@bil.omu.edu.tr>
2014-10-28 21:45:43 +0300

    Create ReplaceUnderscoreWithSpace.js

---
+++ ReplaceUnderscoreWithSpace.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html><script>
+
+function ReplaceUnderscoreWithSpace(string)
+{ 
+    var i,j,s;  
+    var array = string.split("");
+
+    for (i = 0 ; i <= array.length-1 ; i++) 
+    {
+          if (array[i] != "_")
+	            break;			 
+    }
+    for (var j = array.length-1 ; j >= 0 ; j--) 
+    {
+         if (array[j] != "_")
+	            break; 
+	  }
+    for (s = i ; s <= j ; s++)		 
+    {
+          if (array [s] == "_")   	
+	             array[s] = array[s].replace('_',' ');
+    }
+    document.write(array.join(""));
+}
+
+ReplaceUnderscoreWithSpace('__foo____bar__');
+</script>
+<body>
+</body>
+</html>
